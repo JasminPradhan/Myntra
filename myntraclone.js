dfa04a39c849cd300107739fdf51f3b3fcd8d8f5
jasmin <jasminpradhan949@gmail.com>
2022-07-14 00:22:50 +0530

    modification-2

--- myntraclone.js
+++ myntraclone.js
@@ -1,72 +1,71 @@
+//function to add sticky navbar
 
-let slideIndex = 0;
-let slideIndexCurrent = 0;
-showSlides();
-//showSlidesCurrent(slideIndexCurrent);
+const navlist = document.getElementsByClassName(".container");
+const sticky = navlist.offsetTop;
+
+window.addEventListener("scroll", function() {
+    if (window.pageYOffset >= sticky) {
+        navlist.classList.add("sticky");
+    }
+    else {
+        navlist.classList.remove("sticky");
+    }
+})
+
+//function to add image carousel
+
+
+let slideIndex = 0;//for automatic slide display
+let slideIndexCurrent = 0;//for manual slide display
+
+const slides = document.getElementsByClassName("mySlides");
+const dots = document.getElementsByClassName("dot");
+
+showSlides();//for automatic slide display
+
+//manual 
 
 function currentSlide(n) {
-    showSlidesCurrent(slideIndexCurrent = n);
+    //precautionary steps
+    slideIndexCurrent = n;
 
-}
-
-
-/*** CURRENT SLIDE on click ***/
-function currentSlide(n) {
-    showSlidesCurrent(slideIndexCurrent = n);
-
-}
-
-function showSlidesCurrent(n) {
-    //var slideIndex = 1;
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
     if (n > slides.length) { slideIndexCurrent = 1 }
 
     if (n < 0) { slideIndexCurrent = slides.length }
-    for (i = 0; i < slides.length; i++) {
+
+    // default value
+    for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slideIndex++;
-    for (i = 0; i < dots.length; i++) {
+    slideIndex++;//after clicking on button x the next slide be index x+1: this is automatic
+
+    for (let i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
+
     slides[slideIndexCurrent - 1].style.display = "block";
     dots[slideIndexCurrent - 1].className += " active";
     slideIndexCurrent++;
 }
 
 
-/*** SHOW SLIDE with timer ***/
+
+//auto 
+
 function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
+    if (slideIndex > slides.length) { slideIndex = 1 }
+
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    for (i = 0; i < dots.length; i++) {
+
+    for (let i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
+
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 
     setTimeout(showSlides, 5000); // Change image every x seconds
-}
-
-window.onscroll = function () { myFunction() };
-
-const navlist = document.getElementsByClassName(".container");
-const sticky = navlist.offsetTop;
-
-/* Function to stick the nav bar */
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navlist.classList.add("sticky")
-    }
-    else {
-        navlist.classList.remove("sticky");
-    }
 }
